c50d9d482e455db656a3c21a631a5dbce297f310
Alessandro <75171580+predacon2309@users.noreply.github.com>
2023-05-31 19:12:04 +0200

    Update 03_start_replica_container.sh

--- src/script/03_start_replica_container.sh
+++ src/script/03_start_replica_container.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "Avvio i container replica"
-sudo docker run -d --rm -p 27018:27017 --name mongo2 --network mongoCluster mongo:4.4.21 mongod --replSet myReplicaSet --bind_ip localhost,mongo2 
-sudo docker run -d --rm -p 27019:27017 --name mongo3 --network mongoCluster mongo:4.4.21 mongod --replSet myReplicaSet --bind_ip localhost,mongo3
-
-
+sudo docker run -d --rm -p 27018:27017 --name mongo2 --network mongoCluster mongo:5 mongod --replSet myReplicaSet --bind_ip localhost,mongo2
+sudo docker run -d --rm -p 27019:27017 --name mongo3 --network mongoCluster mongo:5 mongod --replSet myReplicaSet --bind_ip localhost,mongo3
