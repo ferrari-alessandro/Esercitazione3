980d082b9598d92d0ccd47769dac275e071f14f7
Alessandro <75171580+predacon2309@users.noreply.github.com>
2023-05-31 19:11:46 +0200

    Update 02_start_first_container.sh

--- src/script/02_start_first_container.sh
+++ src/script/02_start_first_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Avvio il primo container"
-sudo docker run -d --rm -p 27017:27017 --name mongo1 --network mongoCluster mongo:4.4.21 mongod --replSet myReplicaSet --bind_ip localhost,mongo1
+sudo docker run -d --rm -p 27017:27017 --name mongo1 --network mongoCluster mongo:5 mongod --replSet myReplicaSet --bind_ip localhost,mongo1
+
+
 
